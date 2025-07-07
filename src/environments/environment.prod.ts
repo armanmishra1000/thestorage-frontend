7de6f4a041430b5b7d5f4d452379d4687dc5eca6
mitaliborad <emmagarcia91992@gmail.com>
2025-07-07 17:53:19 +0530

    url

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// File: src/environments/environment.prod.ts
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://teletransfer.onrender.com',
+  wsUrl: 'wss://teletransfer.onrender.com/ws_api/ws'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,30 @@
-// export const environment = {
-//   production: true,
-//   // IMPORTANT: Use your actual Render backend URL
-//   apiUrl: 'https://mitali-backend-f8xd.onrender.com/api/v1', 
-//   wsUrl: 'wss://mitali-backend-f8xd.onrender.com/ws_api/ws' 
-// };
+// // export const environment = {
+// //   production: true,
+// //   // IMPORTANT: Use your actual Render backend URL
+// //   apiUrl: 'https://mitali-backend-f8xd.onrender.com/api/v1', 
+// //   wsUrl: 'wss://mitali-backend-f8xd.onrender.com/ws_api/ws' 
+// // };
+
+// // // src/environments/environment.ts
+// // export const environment = {
+// //   production: false,
+// //   apiUrl: 'http://localhost:5000', 
+// //   wsUrl: 'ws://localhost:5000/ws_api/ws'
+// // };
+
 
 // // src/environments/environment.ts
 // export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:5000', 
-//   wsUrl: 'ws://localhost:5000/ws_api/ws'
+//   production: true,
+//   apiUrl: 'https://teletransfer.onrender.com', 
+//   wsUrl: 'wss://teletransfer.onrender.com/ws_api/ws'
 // };
 
 
-// src/environments/environment.ts
+// File: src/environments/environment.ts
+
 export const environment = {
-  production: true,
-  apiUrl: 'https://teletransfer.onrender.com', 
-  wsUrl: 'wss://teletransfer.onrender.com/ws_api/ws'
+  production: false,
+  apiUrl: 'http://localhost:5000',
+  wsUrl: 'ws://localhost:5000/ws_api/ws'
 };
